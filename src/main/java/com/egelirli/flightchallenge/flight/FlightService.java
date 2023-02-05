071ab6435933d9f8975f65cd530ece0673e83c27
egelirli <egelirli@yahoo.com>
2023-02-05 14:46:22 +0300

    Services almost done

--- src/main/java/com/egelirli/flightchallenge/flight/FlightService.java
+++ src/main/java/com/egelirli/flightchallenge/flight/FlightService.java
@@ -1,5 +1,6 @@
 package com.egelirli.flightchallenge.flight;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,7 +10,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.egelirli.flightchallenge.entity.Flight;
+import com.egelirli.flightchallenge.exception.ResourceNotFoundException;
 import com.egelirli.flightchallenge.repository.FlightRepository;
+import com.egelirli.flightchallenge.repository.FlightSeatRepository;
 
 @Service
 @Transactional
@@ -17,12 +20,26 @@ public class FlightService {
 	private Logger logger = LoggerFactory.getLogger(FlightService.class);
 	
 	private FlightRepository flightRepo;
+
+	private FlightSeatRepository seatRepo;
 	
-	public FlightService(FlightRepository flightRepo){
+	public FlightService(FlightRepository flightRepo,
+						 FlightSeatRepository seatRepo){
 		this.flightRepo = flightRepo;
+		this.seatRepo = seatRepo;
 	}
 	
+	public Flight findFlightByNumber(String flighNumber) 
+							throws ResourceNotFoundException {
+		return flightRepo.findById(flighNumber).
+				orElseThrow(()->new ResourceNotFoundException(
+					"Flight Not Found " + flighNumber));
+		
+	}
+
+	
 	public boolean add(String flightNumber, 
+					   BigDecimal price,
 					   String airlineName, 
 					   String origin,
 					   String destin) {
@@ -37,6 +54,7 @@ public class FlightService {
 		
 		Flight flight = new Flight();
 		flight.setFlightNumber(flightNumber);
+		flight.setPrice(price);
 		flight.setAirlineName(airlineName);
 		flight.setOrigin(origin);
 		flight.setDestin(destin);
@@ -54,26 +72,34 @@ public class FlightService {
 			logger.error("In remove - could not find  flight({}) to remove!", flightNumber);
 			return false;
 		}else {
+			
 			flightRepo.delete(fl.get());
-			logger.debug("In remove - removed flight - flightNumber: {} ",flightNumber);
+			seatRepo.deleteByFlightNumber(flightNumber);
+			logger.debug("In remove - removed flight  and seats - flightNumber: {} ",flightNumber);
 
 			return true;
 		}
 	}
 
-	public boolean update(Flight flToSave) {
-		Optional<Flight> fl = flightRepo.findById(flToSave.getFlightNumber());
-		if(fl.isEmpty()) {
-			logger.error("In update - could not find  flight({}) to update!", 
-														flToSave.getFlightNumber());
-			return false;
-		}else {
-			flightRepo.save(flToSave);
-			logger.debug("In update - Updated flight - flightNumber: {} ",
-													flToSave.getFlightNumber());
-			return true;
-		}
+	public void update(Flight flToSave) throws ResourceNotFoundException {
+		
+		Flight fl = flightRepo.findById(flToSave.getFlightNumber()).
+				orElseThrow(()->new ResourceNotFoundException(
+					"Flight Not Found " + flToSave.getFlightNumber()));
+		flightRepo.save(flToSave);
+//		Optional<Flight> fl = flightRepo.findById(flToSave.getFlightNumber());
+//		if(fl.isEmpty()) {
+//			logger.error("In update - could not find  flight({}) to update!", 
+//														flToSave.getFlightNumber());
+//			return false;
+//		}else {
+//			flightRepo.save(flToSave);
+//			logger.debug("In update - Updated flight - flightNumber: {} ",
+//													flToSave.getFlightNumber());
+//			return true;
+//		}
 		
 	}
+
 	
 }
--- src/main/java/com/egelirli/flightchallenge/flight/SeatService.java
+++ src/main/java/com/egelirli/flightchallenge/flight/SeatService.java
@@ -1,6 +1,7 @@
 package com.egelirli.flightchallenge.flight;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.egelirli.flightchallenge.entity.Flight;
 import com.egelirli.flightchallenge.entity.FlightSeat;
+import com.egelirli.flightchallenge.exception.ResourceNotFoundException;
 import com.egelirli.flightchallenge.repository.FlightRepository;
 import com.egelirli.flightchallenge.repository.FlightSeatRepository;
 
@@ -30,28 +32,26 @@ public class SeatService {
 	}
 	
 	public boolean addSeat(String flightNumber, 
-						   String seatNumber,
-						   BigDecimal price) {
+						   String seatNumber) {
 		logger.debug("In addSeat -  flightNumber: {} seatNumber: {}"
 				 , flightNumber, seatNumber);
 		
-
-		Optional<FlightSeat> st = seatRepo.findById(seatNumber);
-		if(!st.isEmpty()) {
-				logger.error("In addSeat - seat({}) already exists !", seatNumber);
-				return false;
-		 }
-
-		Optional<Flight> fl = flightRepo.findById(flightNumber);
-		if(fl.isEmpty()) {
+		Optional<Flight> flight = flightRepo.findById(flightNumber);
+		if(flight.isEmpty()) {
 				logger.error("In addSeat - flight({}) not exists !", flightNumber);
 				return false;
 		 }
+
+//		Optional<FlightSeat> st = flight.get().findSeat(seatNumber);
+//		if(!st.isEmpty()) {
+//				logger.error("In addSeat - seat({}) already exists !", seatNumber);
+//				return false;
+//		 }
+
 			
 		FlightSeat seat = new FlightSeat();
-		seat.setFlightNumber(flightNumber);
+		seat.setFlight(flight.get());
 		seat.setSeatNumber(seatNumber);
-		seat.setPrice(price);
 		seat.setAvailable(true);
 		seatRepo.save(seat);
 		
@@ -63,22 +63,88 @@ public class SeatService {
 		
 	}
 	
-	public boolean removeSeat(String flightNumber, 
-							  String seatNumber) {
+//	public boolean removeSeat(String flightNumber, 
+//							  String seatNumber) {
+//		
+//		logger.debug("In removeSeat -  flightNumber: {} seatNumber: {}"
+//				 , flightNumber, seatNumber);
+//		
+//		Optional<Flight> flight = flightRepo.findById(flightNumber);
+//		if(flight.isEmpty()) {
+//				logger.error("In removeSeat - flight({}) not exists !", flightNumber);
+//				return false;
+//		 }
+//		
+//		
+//		seatRepo.findAllById(null)
+//		seatRepo.delete(seat);
+//	
+//	}
+
+	public  void updateSeat(int  seatId, 
+							boolean isAvailable) throws ResourceNotFoundException {
 		
-		logger.debug("In removeSeat -  flightNumber: {} seatNumber: {}"
-				 , flightNumber, seatNumber);
+		logger.debug("In updateSeat - seatId: {} "
+				+ "isAvailable : {}", seatId,isAvailable );
 		
-		seatRepo.findAllById(null)
-		seatRepo.delete(seat);
-	
+		FlightSeat fs =  this.seatRepo.findById(seatId).
+				 orElseThrow(()-> new ResourceNotFoundException("Seat not exists -  seatId : " + seatId));
+		
+		//fs.setPrice(price);
+		fs.setAvailable(isAvailable);
+		this.seatRepo.save(fs);
 	}
 	
-	public  boolean updateSeat(String fligntNumber, 
-			  				  String seatNumber,
-			  				  boolean isAvailable) {
-
-    }
 	
-	 
+	public boolean isSeatAvailable(int  seatId) throws ResourceNotFoundException {
+		FlightSeat fs =  this.seatRepo.findById(seatId).
+				 orElseThrow(()-> 
+				    new ResourceNotFoundException("Seat not exists- seatId : " + seatId));
+		
+		//fs.setPrice(price);
+		return fs.getAvailable();
+		
+	}
+	
+	public void removeSeat(int  seatId) {
+
+		logger.debug("In removeSeat - seatId: {}", seatId);
+		this.seatRepo.deleteById(seatId);
+		logger.debug("In removeSeat - removed seatId: {}", seatId);
+
+	}
+	
+	public void removeAllSeatsOfFlight(String flightNumber) 
+								throws ResourceNotFoundException {
+		logger.debug("In removeAllSeatsOfFlight - flightNumber: {}", flightNumber);
+		if(!this.flightRepo.existsById(flightNumber)) {
+			throw new ResourceNotFoundException(
+					"Flight not exists -  flightNumber : " + flightNumber);
+		}
+		
+		this.seatRepo.deleteByFlightNumber(flightNumber);
+	}
+	
+	public List<FlightSeat> getAvailableSeats(String flightNumber ) 
+											throws ResourceNotFoundException{
+		logger.debug("In getAvailableSeats - flightNumber: {}", flightNumber);
+		Flight fl = flightRepo.findById(flightNumber).
+				orElseThrow(()->new ResourceNotFoundException(
+					"Flight Not Found " + flightNumber));
+	
+		return this.seatRepo.findByIsAvailable(true);
+	}
+	
+	
+	public Flight getFlightWithAvailableSeats(String flightNumber) 
+										throws ResourceNotFoundException{
+		logger.debug("In getFlightWithAvailableSeats - flightNumber: {}", flightNumber);  
+		Flight flight = flightRepo.findById(flightNumber).
+				orElseThrow(()->new ResourceNotFoundException(
+					"Flight Not Found " + flightNumber));
+	
+		flight.setSeatList(this.seatRepo.findByIsAvailable(true));
+		return flight;		
+		
+	}
 }
