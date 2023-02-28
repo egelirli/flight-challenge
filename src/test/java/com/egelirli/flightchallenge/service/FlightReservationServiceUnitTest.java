fe3fc5b521d7a6d5e99edb401e706297397e5539
egelirli <egelirli@havelsan.com.tr>
2023-02-28 15:05:33 +0300

    Birim test tamam gibi

--- src/test/java/com/egelirli/flightchallenge/service/FlightReservationServiceUnitTest.java
+++ src/test/java/com/egelirli/flightchallenge/service/FlightReservationServiceUnitTest.java
@@ -1,19 +1,30 @@
 package com.egelirli.flightchallenge.service;
 
+import static org.junit.Assert.*;
+
 import java.math.BigDecimal;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.egelirli.flightchallenge.entity.Flight;
 import com.egelirli.flightchallenge.entity.FlightSeat;
+import com.egelirli.flightchallenge.exception.ResourceNotFoundException;
+import com.egelirli.flightchallenge.flight.FlightReservationService;
 import com.egelirli.flightchallenge.flight.FlightService;
 import com.egelirli.flightchallenge.flight.SeatService;
 
+@EnableAutoConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class FlightReservationServiceUnitTest {
 	private static Logger logger = 
 			LoggerFactory.getLogger(FlightReservationServiceUnitTest.class);
@@ -21,20 +32,23 @@ public class FlightReservationServiceUnitTest {
 	 private static Flight testFlight1;
 	 private static String testFlNumber1 = "ERD-001";
 	
-	 private static String  seat1 = "1A", seat2= "1B", seat3 = "1C";
+	// private static String  seat1 = "1A", seat2= "1B", seat3 = "1C";
 		
 	 private static FlightSeat fs1,fs2,fs3; 
 	 
 	 
-	private static  Flight testFlight2;
-	private static  String testFlNumber2 = "ERD-002";
+	//private static  Flight testFlight2;
+	//private static  String testFlNumber2 = "ERD-002";
 
 	@Autowired
 	private FlightService flightService;
  
 	@Autowired
-	private static SeatService seatService;
- 
+	private SeatService seatService;
+	
+	@Autowired
+	private FlightReservationService reservationService;
+	
 	@BeforeAll
 	public static void executeBeforeAll() {
 		logger.debug("In onceExecutedBeforeAll");
@@ -46,12 +60,13 @@ public class FlightReservationServiceUnitTest {
 		 testFlight1.setAirlineName("ERD-HY");
 		 testFlight1.setOrigin("IST");
 		 testFlight1.setOrigin("IZMIR");
-
-		String  seat1 = "1A", seat2= "1B", seat3 = "1C";
+		 
+		 
+//		String  seat1 = "1A", seat2= "1B", seat3 = "1C";
 		
-		fs1 = seatService.addSeat(testFlight1.getFlightNumber(), seat1);
-		fs2 = seatService.addSeat(testFlight1.getFlightNumber(), seat2);
-		fs3 = seatService.addSeat(testFlight1.getFlightNumber(), seat3);
+//		fs1 = seatService.addSeat(testFlight1.getFlightNumber(), seat1);
+//		fs2 = seatService.addSeat(testFlight1.getFlightNumber(), seat2);
+//		fs3 = seatService.addSeat(testFlight1.getFlightNumber(), seat3);
 
 		
 	}
@@ -68,7 +83,34 @@ public class FlightReservationServiceUnitTest {
 	public void testReserveSeat() {
 		logger.debug("In testReserveSeat ");
 		
-		assertTrue(seatService.reserveSeat(testFlight1.));
+		try {
+			String  seat1 = "1A", seat2= "1B", seat3 = "1C";
+			
+			assertTrue(flightService.add(testFlight1));
+			fs1 = seatService.addSeat(testFlight1.getFlightNumber(), seat1);
+			fs2 = seatService.addSeat(testFlight1.getFlightNumber(), seat2);
+			fs3 = seatService.addSeat(testFlight1.getFlightNumber(), seat3);
+			
+			assertTrue(
+					reservationService.reserveSeat(testFlight1.getFlightNumber(), fs1.getId()));
+			assertFalse(
+					reservationService.reserveSeat(testFlight1.getFlightNumber(), fs1.getId()));
+			assertFalse(
+					reservationService.reserveSeat(testFlight1.getFlightNumber(), fs1.getId()));
+
+			
+			assertTrue(
+					reservationService.reserveSeat(testFlight1.getFlightNumber(), fs2.getId()));
+		
+			
+			assertTrue(
+					reservationService.reserveSeat(testFlight1.getFlightNumber(), fs3.getId()));
+
+			
+			
+		} catch (ResourceNotFoundException e) {
+			fail("Exception : " + e.getMessage());
+		}
 		
 		
 	}
